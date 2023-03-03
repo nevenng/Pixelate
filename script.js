8dc619a52a908ec38e9e2db6ac4edf20b5693362
Neven Nguyen <nevenng@gmail.com>
2023-03-02 20:53:42 -0800

    added code

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 // Your code here
+
+const newTable = document.getElementsByTagName("table")[0];
+
+const makeRow = () => {
+    const tr = document.createElement('tr');
+    for(let i = 0; i < 20; i++){
+        const td = document.createElement('td');
+        tr.appendChild(td);
+    }
+    newTable.appendChild(tr);
+}
+
+const newRow = document.getElementById("add-row");
+newRow.addEventListener("click", makeRow);
+
+
+
+
+const colorize = (event) => {
+    let target = event.target;
+    if(target.className.length){
+        target.className = ''
+    }
+    else {
+        target.className = color;
+    }
+
+
+
+}
+
+newTable.addEventListener("click",colorize);
+
+let color = "skyblue";
+
+const selector = document.getElementsByTagName("select")[0];
+
+selector.addEventListener("change",function(event){
+    color = event.target.value;
+});
+
+
+
+const newFunction = (event) => {
+    color = event.target.value;
+}
+selector.addEventListener("change",newFunction);
